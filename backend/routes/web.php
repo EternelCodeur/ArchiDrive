c584878854a23d0033265de7ff50bea6742fecca
ETERNEL CODEUR <eternelcodeur@gmail.com>
2025-12-23 10:28:20 +0100

    Deploiement sur LWS Séparé le front du Backend

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,11 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
-// routes/web.php
-Route::get('/{any}', function () {
-    $path = public_path('build/index.html');
-
-    return new BinaryFileResponse($path);
-})->where('any', '^(?!api).*$');
+Route::get('/', function () {
+    return view('welcome');
+});
