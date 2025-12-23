eaf127bf5e0c01727cdce419928e3156b5073b6e
ETERNEL CODEUR <eternelcodeur@gmail.com>
2025-12-23 08:53:11 +0100

    Deploiement sur LWS 4

--- multi-share-vault/vite.config.ts
+++ multi-share-vault/vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
-  //const apiUrl = 'http://127.0.0.1:8000';
+
   const apiUrl = env.VITE_API_PROXY_TARGET || (mode === "development" ? "http://127.0.0.1:8000" : "https://www.api.archi-drive.ga");
   return {
     server: {
